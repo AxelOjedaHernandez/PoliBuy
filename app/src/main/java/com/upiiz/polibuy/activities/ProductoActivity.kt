7aa70f7864cb7e7bf87cda5ae10b1c19aa8ae5b3
AxelOjedaHernandez <axelojedahernandez64@gmail.com>
2025-01-15 17:05:07 -0600

    lista productos

---
+++ app/src/main/java/com/upiiz/polibuy/activities/ProductoActivity.kt
@@ -0,0 +1,81 @@
+package com.upiiz.polibuy.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectofinal.data.Producto
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
+import com.upiiz.polibuy.R
+import com.upiiz.polibuy.adapters.ProductAdapter
+
+class ProductoActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var productAdapter: ProductAdapter
+    private val productList =  mutableListOf<Producto>()
+    private val idUsuario: String=""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_producto)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        recyclerView = findViewById(R.id.rvProductosListaProductos)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        productAdapter = ProductAdapter(
+            productList, addCar = {producto -> productoEspecifico(producto) }
+        )
+        recyclerView.adapter = productAdapter
+        cargarProductos()
+    }
+
+    private fun productoEspecifico(producto: Producto){
+        val productoIntent = Intent(this@ProductoActivity, ProductoEspecificoActivity::class.java)
+        productoIntent.putExtra("idProducto", producto.id)
+        productoIntent.putExtra("nombreProducto", producto.nombre)
+        productoIntent.putExtra("descripcionroducto", producto.descripcion)
+        productoIntent.putExtra("precioroducto", producto.precio)
+        productoIntent.putExtra("cantidadroducto", producto.cantidad)
+        productoIntent.putExtra("idUsuario", idUsuario)
+        startActivity(productoIntent)
+    }
+
+    private fun cargarProductos(){
+        val database = Firebase.database
+        val productRef = database.getReference("Producto")
+
+        productRef.get().addOnSuccessListener { dataSnapshot ->
+            productList.clear()
+            for (snapshot in dataSnapshot.children) {
+                val id = snapshot.child("id").value.toString()
+                val nombre = snapshot.child("nombre").value.toString()
+                val precio = snapshot.child("precio").value.toString().toIntOrNull()
+                val cantidad = snapshot.child("cantidad").value.toString().toIntOrNull()
+
+                val producto = Producto(
+                    id = id,
+                    nombre = nombre,
+                    precio = precio,
+                    cantidad = cantidad
+                )
+
+                productList.add(producto)
+            }
+
+            productAdapter.notifyDataSetChanged()
+        }.addOnFailureListener {
+            Toast.makeText(this, "Error al cargar contactos", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/upiiz/polibuy/activities/ProductoEspecificoActivity.kt
+++ app/src/main/java/com/upiiz/polibuy/activities/ProductoEspecificoActivity.kt
@@ -1,16 +1,17 @@
-package com.upiiz.polibuy
+package com.upiiz.polibuy.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.upiiz.polibuy.R
 
-class ProductoActivity : AppCompatActivity() {
+class ProductoEspecificoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_producto)
+        setContentView(R.layout.activity_producto_especifico)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
