ec47578be95353e9233d2ee7e06a9dd82fc06949
AxelOjedaHernandez <axelojedahernandez64@gmail.com>
2025-01-15 14:49:15 -0600

    invitacion

---
+++ app/src/main/java/com/upiiz/polibuy/data/Usuario.kt
@@ -0,0 +1,8 @@
+package com.example.proyectofinal.data
+
+data class Usuario(
+    val id: String?=null,
+    val clave: String?=null,
+    val nombre: String?=null,
+    val correo: String?=null,
+)
